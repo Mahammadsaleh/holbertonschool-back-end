aafb7375a29199817e59667ad5484db3c4932b24
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-04-24 19:26:16 +0400

    try

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,26 +1,24 @@
-#!/usr/bin/python3
-"""Gather data from an API"""
 import csv
 import json
 import requests
 import sys
 
+#!/usr/bin/python3
+"""Gather data from an API"""
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
-        res = requests.get('https://jsonplaceholder.typicode.com/users/' +
-                           f'{sys.argv[1]}/todos')
+        user_id = sys.argv[1]
+        res = requests.get(f'https://jsonplaceholder.typicode.com/users/{user_id}/todos')
 
         response_usr_todo = json.loads(res.text)
-        todo_all_len = len(response_usr_todo)
-        user = requests.get('https://jsonplaceholder.typicode.com/users/' +
-                            f'{sys.argv[1]}')
+        user = requests.get(f'https://jsonplaceholder.typicode.com/users/{user_id}')
         user_response = json.loads(user.text)
         user_name = user_response["username"]
-        with open(f"{sys.argv[1]}.csv", "w") as csv_file:
-            writer = csv.writer(csv_file, delimiter=',',
-                                quotechar='"', quoting=csv.QUOTE_ALL)
+        with open(f"{user_id}.csv", "w") as csv_file:
+            writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+            writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
             for todo in response_usr_todo:
-                csv_list = [str(todo['userId']), str(user_name),
-                            str(todo['completed']), str(todo['title'])]
+                csv_list = [str(todo['userId']), str(user_name), str(todo['completed']), str(todo['title'])]
                 writer.writerow(csv_list)
